92732c7f00c83375047025b1fe0ab7789c387446
Marcelo Schneider <schneider.fei@gmail.com>
2021-12-09 13:52:39 -0300

    Atualiza setup.py #17
    Adiciona configurações em setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
+import os
 import setuptools
+import os
 from setuptools import setup
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(
     name="django_oauth_usp",
-    version="0.0.1",
-    url = 'https://github.com/uspdev/django-oauth-usp.git',
+    version="1.1.0",
+    url='https://github.com/uspdev/django-oauth-usp.git',
     description="Django oauth usp package",
-    author="Marcelo",
+    long_description=open(README).read(),
+    author="Marcelo Schneider",
     author_email="schneider.fei@gmail.com",
+    license="MIT",
+    zip_safe=False,
+    platforms='any',
+    include_package_data=True,
     packages=setuptools.find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2.5',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ],
     install_requires=[
         'Django>=3.2'
         'Authlib>=0.15.2',
-        'requests>=2.24.0',
-
+        'requests>=2.24.0'
     ],
 )
