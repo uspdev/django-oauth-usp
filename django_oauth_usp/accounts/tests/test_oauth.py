ad25b7a3a8e7d5f2ac7489cbc4dc183c3b8b0f5a
Marcelo <schneider.fei@gmail.com>
2023-07-01 14:23:30 -0300

    Atualiza o pacote Authlib (#22)
    * Corrige testes #21
    
    Corrige testes devido a alteração da versão do pacote Authlib
    
    * Adiciona pyproject e poetry.lock #21
    
    * Adiciona classifiers #21
    
    Adiciona classifiers em pyproject.toml
    
    * Utiliza poetry para gerenciar pacotes #21
    
    * Atualiza requimentents.txt #21
    
    * Configura o Tox #21
    
    * Atualiza o .gitignore #21
    
    * Remove setup.py e setup.cfg #21
    
    setup.py e setup.cfg não são mais necessários
    
    * Remove MANIFEST.IN #21
    
    MANITEST.IN não é mais necessário com a utilização do pyproject
    
    * Configura github workflow #21
    
    ---------
    
    Co-authored-by: Marcelo Schneider <schenider.fei@gmail.com>
    
    Closes #21

--- django_oauth_usp/accounts/tests/test_oauth.py
+++ django_oauth_usp/accounts/tests/test_oauth.py
@@ -1,8 +1,8 @@
 import json
 from django.test import TestCase
-from authlib.integrations.django_client.integration import DjangoRemoteApp
 from django.http import HttpRequest, QueryDict
 from django.conf import settings
+from django.contrib.sessions.backends.db import SessionStore
 
 from ..oauth import OAuthUsp
 from .mock import mock_oauth, token, resource, verifier
@@ -14,9 +14,6 @@ class OAuthUspTest(TestCase):
 
     def test_has_attribute_oauth_usp(self):
         self.assertTrue(hasattr(self.obj, 'oauth_usp'))
-
-    def test_oauth_usp_instance(self):
-        self.assertIsInstance(self.obj.oauth_usp, DjangoRemoteApp)
 
     @mock_oauth
     def test_get_authorize_redirect(self):
@@ -31,7 +28,18 @@ class OAuthUspTest(TestCase):
         request = HttpRequest()
         query = QueryDict(
             'oauth_token=12345oauth&oauth_verifier=12345veriifer')
+
         request.GET = query
+
+        session = SessionStore()
+        setattr(request, 'session', session)
+
+        request_token = dict(
+            oauth_token='token123', oauth_token_secret='oauth_token_secret123',
+            oauth_verifier='verifier123')
+        data = {"data": {"request_token": request_token}}
+        request.session['_state_usp_12345oauth'] = data
+
         resp = self.obj.get_resource(request)
         expected = json.loads(resource)
         self.assertDictEqual(expected, resp)
